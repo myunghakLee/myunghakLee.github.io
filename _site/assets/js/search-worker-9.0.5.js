9cabac1b2ece8b57fc991c9cf50b24f405c19de8
myunghakLee <mh9716@naver.com>
2021-11-12 21:40:07 +0900

    delete tmp

--- _site/assets/js/search-worker-9.0.5.js
+++ _site/assets/js/search-worker-9.0.5.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2021-11-12T02:45:55+09:00';
+const INDEX_KEY = 'index--2021-11-12T21:38:49+09:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
--- _site/mdChanger_for_Typora.py
+++ _site/mdChanger_for_Typora.py
@@ -5,6 +5,8 @@ from tkinter import filedialog
 from tkinter import messagebox
 from os import listdir
 from os.path import isfile, join
+from shutil import copyfile
+import shutil
 
 root= tk.Tk()
 
@@ -15,21 +17,27 @@ label1 = tk.Label(root, text='md-changer\nfor-typoraImage', bg = 'white')
 label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 60, window=label1)
 
-link1 = "C:\\Users\\myunghak\\AppData\\Roaming\\Typora\\typora-user-images\\"
-link2 = "https://github.com/myunghakLee/GIT_BLOG_IMAGE/blob/master/"
+link1 = "C:\\Users\\mh9716\\AppData\\Roaming\\Typora\\typora-user-images\\"
+link2 = "https://github.com/myunghakLee/myunghakLee.github.io/blob/main/blog_images/"
 def getFile ():
     read_path = ""
     write_path = ""
     read_path = filedialog.askopenfilename()
     print("Read : "+ read_path)
-
-    write_path = read_path[:-3] + "-changed" + read_path[-3:]
+    shutil.copy2(read_path, read_path + ".tmp")
+    write_path = read_path[:-3] + read_path[-3:]
     out = open(write_path, 'wt', encoding='UTF8')
-    with open(read_path,'rt', encoding='UTF8') as fp:
+    with open(read_path+ ".tmp",'rt', encoding='UTF8') as fp:
         while True:
             line = fp.readline()
             print(line)
             if not line: break
+
+            if "typora-user-images" in line:
+                file_name = line.split("\\")[-1][:-1]
+                print((link1 + file_name).replace("\\\\", "\\"))
+                shutil.copy2(link1 + file_name, "/gitpage/myunghakLee.github.io/blog_images/")
+
             line1 = line.replace(link1,link2)
             line2 = line1.replace(".png",".png?raw=tru")
             print(line2)
